9a71180f619c7df6bd9e03e3d3df4bef11466345
Antigenic Study <106180822+AntigenicStudy@users.noreply.github.com>
2024-08-27 15:43:43 +0800

    Update embeddingDistance.py

--- code/embeddingDistance.py
+++ code/embeddingDistance.py
@@ -1,48 +1,47 @@
+#Calculate cosine similarity between two embedding vectors of protein sequences.
+import cv2
+import cupy as np
+import math
+import time
 import os
 import sys
-import math
-import numpy as np
 
 def main():
     input_path = sys.argv[1]
     output_file = sys.argv[2]
-    
-    matrix_path = input_path
 
-    f2 = open(output_file,'w')
+    matrix_path = input_path
 
     matrix_path_list = os.listdir(matrix_path)
     matrix_path_list.sort(key=lambda x:int(x[10:-4]))
 
-    virus_count = -1
+    f1 = open(output_file,'w')
+
     for matrix_file_outer in matrix_path_list:
         f = open(matrix_path + '/' + matrix_file_outer)
-        print(f.name)
         a = np.loadtxt(f) 
-        s_a = a.shape
-        print(a.shape)  
-        virus_count = virus_count + 1 
-            
+        s_a = a.shape 
+        
         for matrix_file_inner in matrix_path_list:
             f1 = open(matrix_path + '/' + matrix_file_inner)
-            print(f1.name)
-            
             b = np.loadtxt(f1)
             s_b = b.shape
-            print(b.shape)  
             row = min(s_a[0], s_b[0])
                     
-            grid = np.zeros(shape=(568, 1))
-            grid = np.sum(pow(a[:row,:] - b[:row,:],2),axis=1)
-            print(grid.shape)       
-        
+            grid = np.zeros(shape=(row, 1))
+
+            for i in range(row):
+                vector_1 = a[i, :]
+                vector_2 = b[i, :]
+                cosine = (float(np.dot(vector_1, vector_2)) / (np.linalg.norm(vector_1) * np.linalg.norm(vector_2)))
+                normalized_dist = (1-cosine)/ 2.0
+                grid[i] = normalized_dist
             grid = grid.T
-            np.savetxt(f2, grid, fmt='%f', delimiter=' ', newline=' ')
+            np.savetxt(f1, grid, fmt='%f', delimiter=',', newline=' ')
         
-            f2.write('\n')
-            
-    f2.close()        
-   
-    
+            f1.write('\n')
+    f1.close()        
+
+
 if __name__ == '__main__':
     main()
